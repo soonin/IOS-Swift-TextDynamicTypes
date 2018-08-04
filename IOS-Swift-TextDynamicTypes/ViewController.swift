7f3a725cbef9b4a1077a206722be0066f27cf904
Pooya Hatami <pooya@node-1w7jr9qro06sw04hcipbybhw2.ipv6.telus.net>
2018-08-03 17:09:37 -0700

    done

--- IOS-Swift-TextDynamicTypes/ViewController.swift
+++ IOS-Swift-TextDynamicTypes/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var subheadlineLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var footnoteLabel: UILabel!
+    @IBOutlet weak var caption1Label: UILabel!
+    @IBOutlet weak var caption2Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        updateFonts()
     }
 
 
+    func updateFonts() {
+        headlineLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        subheadlineLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
+        bodyLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+        footnoteLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.footnote)
+        caption1Label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.caption1)
+        caption2Label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.caption2)
+    }
+    
 }
 
